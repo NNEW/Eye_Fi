b8946a98be5aa86448a8b85bdbe7cdf5b12ef97b
Park Changho <pch4463@gmail.com>
2015-06-10 02:11:09 +0900

    Implement User's name and toast alert

--- app/src/main/java/com/swstudio/dunkin/eye_fi/CallActivity.java
+++ app/src/main/java/com/swstudio/dunkin/eye_fi/CallActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -25,13 +26,13 @@ import android.widget.EditText;
 import android.widget.Filter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Date;
 
 public class CallActivity extends Activity {
 
-    public String myId = "현상원";
 
     String dbName = "vltList.db";
     String tableName = "vltListTable";
@@ -46,6 +47,11 @@ public class CallActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call);
+
+        SharedPreferences prefs = getSharedPreferences("PrefName", Context.MODE_PRIVATE);
+        String text = prefs.getString("Name", "");
+        final EditText edit = (EditText)findViewById(R.id.Name);
+        edit.setText(text);
 
         // Video call with phoneNumber.
         /*findViewById(R.id.callBtn).setOnClickListener(
@@ -63,27 +69,37 @@ public class CallActivity extends Activity {
         findViewById(R.id.callBtn).setOnClickListener(
             new View.OnClickListener() {
                 public void onClick(View v) {
-                    SmsManager mSmsManager = SmsManager.getDefault();
-                    ArrayList<String> numbers = new ArrayList<String>();
+                    final EditText edit = (EditText)findViewById(R.id.Name);
+                    String myId = edit.getText().toString();
+                    if(myId.equals("")) {
+                        Toast toast = Toast.makeText(getApplicationContext(), "도움요청을 하지 못했습니다.\n사용자의 이름을 설정해주십시오.", Toast.LENGTH_SHORT);
+                        toast.show();
+                    } else {
+                        SmsManager mSmsManager = SmsManager.getDefault();
+                        ArrayList<String> numbers = new ArrayList<String>();
 
-                    String sql = "select * from " + tableName + ";";
-                    GetGuideNum.db = openOrCreateDatabase(dbName, dbMode, null);
-                    Cursor results = GetGuideNum.db.rawQuery(sql, null);
+                        String sql = "select * from " + tableName + ";";
+                        GetGuideNum.db = openOrCreateDatabase(dbName, dbMode, null);
+                        Cursor results = GetGuideNum.db.rawQuery(sql, null);
 
-                    results.moveToFirst();
+                        results.moveToFirst();
 
-                    while(!results.isAfterLast()){
-                        String number = results.getString(2);
+                        while (!results.isAfterLast()) {
+                            String number = results.getString(2);
 
-                        numbers.add(number);
+                            numbers.add(number);
 
-                        results.moveToNext();
-                    }
-                    results.close();
+                            results.moveToNext();
+                        }
+                        results.close();
 
-                    for(int i = 0; i < numbers.size(); i++) {
-                        String smsText = "[EYE-FI] " + myId + "님으로부터 도움요청.";
-                        mSmsManager.sendTextMessage(numbers.get(i),null, smsText, null, null);
+                        for (int i = 0; i < numbers.size(); i++) {
+                            String smsText = "[EYE-FI] " + myId + "님으로부터 도움요청.";
+                            mSmsManager.sendTextMessage(numbers.get(i), null, smsText, null, null);
+                        }
+
+                        Toast toast = Toast.makeText(getApplicationContext(), numbers.size() + "명에게 도움요청을 했습니다.", Toast.LENGTH_SHORT);
+                        toast.show();
                     }
                 }
         });
@@ -103,5 +119,18 @@ public class CallActivity extends Activity {
                 startActivity(new Intent(getApplication(), dbList.class));
             }
         });
+    }
+
+    protected void onStop() {
+        super.onStop();
+        final EditText edit = (EditText)findViewById(R.id.Name);
+        String text = edit.getText().toString();
+
+        // 데이타를저장합니다.
+        SharedPreferences prefs = getSharedPreferences("PrefName", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("Name", text);
+        editor.commit();
+
     }
 }
